1244abe077c856a02281f079a31427ad22123e04
Mattt <mattt@me.com>
2025-07-24 13:57:49 -0700

    Transfer project from Loopwork (#4)

--- Formula/olleh.rb
+++ Formula/olleh.rb
@@ -1,9 +1,9 @@
 class Olleh < Formula
-    desc "An Ollama-compatible CLI for Apple's Foundation Models"
-    homepage "https://github.com/loopwork/olleh"
-    url "https://github.com/loopwork/olleh/archive/refs/tags/1.0.0.tar.gz"
+    desc "Ollama-compatible CLI for Apple's Foundation Models"
+    homepage "https://github.com/mattt/olleh"
+    url "https://github.com/mattt/olleh/archive/refs/tags/1.0.0.tar.gz"
     sha256 "e7f3c233d94fbccd9ab86eb3e3400933cc9f62512fb7b76e4d7f05a7df7453fe"
-    license "Apache-2.0"
+    license "MIT"
   
     depends_on xcode: [">= 26.0", :build]
     depends_on macos: [:tahoe]
